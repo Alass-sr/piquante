707b2da49794fda0c3ee530f4ba82e065b847d4d
Alass-sr <sarralass88@gmail.com>
2022-09-16 12:02:31 +0200

    ajout token et crypto-js

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -2,13 +2,12 @@ const Sauce = require("../models/sauces");
 const fs = require("fs");
 
 exports.createSauce = (req, res, next) => {
-
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   delete sauceObject._userId;
   const sauce = new Sauce({
     ...sauceObject,
-    userId: res.auth.userId,
+    userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
@@ -23,7 +22,6 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.getAllSauces = (req, res, next) => {
-  
   Sauce.find()
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
@@ -51,12 +49,13 @@ exports.modifySauce = (req, res, next) => {
       if (sauce.userId != req.auth.userId) {
         res.statuts(401).json({ message: "Non autorié" });
       } else {
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { ...sauceObject, _id: req.params.id }
-        )
-          .then(() => res.status(200).json({ message: "Objet modifié !" }))
-          .catch((error) => res.status(401).json({ error }));
+        const filename = thing.imageUrl.split('/images')[1]
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({_id: req.params.id})
+            .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+            .catch(error => res.status(401).json({ error }));
+        });
+          
       }
     })
     .catch((error) => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,20 @@
 const bcrypt = require("bcrypt");
-
+const jwt = require('jsonwebtoken');
+const cryptojs = require("crypto-js");
 const User = require("../models/User");
 
 const log = require("../utils/winston");
 
 exports.signup = (req, res, next) => {
+
+  const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, "CLE_SECRET").toString();
+  console.log("--->CONTENU: emailCryptoJs");
+  console.log(emailCryptoJs);
+
   bcrypt.hash(req.body.password, 10)
   .then(hash => {
     const user = new User({
-      email: req.body.email,
+      email: emailCryptoJs,
       password: hash
     });
     user.save()
@@ -33,25 +39,24 @@ exports.login = (req, res, next) => {
       if (!user) {
         log.error(`error incorrect user => ${req.body.email}`);
         return res.status(401).json({error: "Utilisateur non trouvé !"});
-      } else {
-        bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            if (!valid) {
-              res.status(401).json({message: 'Paire identif/mot de passe incorrecte'})
-            } else {
-              res.status(200).json({
-                userId: user._id,
-                token: 'TOKEN'
-              });
-            }
-        })
-        .catch(error => {
-          res.status(500).json( { error } );
-        })
-      }
-    })
-    .catch(error => {
-      res.status(500).json( {error} );
-    })
+      } 
+      bcrypt
+      .compare(req.body.password, user.password)
+      .then((valid) => {
+        if (!valid) {
+          return res.status(401).json({ error: "Mot de passe incorrect !" });
+        }
+        res.status(200).json({
+          userId: user._id,
+          token: jwt.sign(
+            { userId: user._id },
+             "RANDOM_TOKEN_SECRET", 
+             { expiresIn: '24h' }
+             )
+        });
+      })
+      .catch((error) => res.status(500).json({ error }));
+  })
+  .catch((error) => res.status(500).json({ error }));
 
 };
